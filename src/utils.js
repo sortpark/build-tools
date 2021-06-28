b708046ee69aa77fad8d6399df3468c22278a69c
Erik Koopmans <erik@erik-koopmans.com>
2021-06-27 21:35:36 -0400

    Add env vars for version prefix

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,17 @@
 import { promisify } from 'util';
 import { exec as execOrig } from 'child_process';
 import { readFileSync } from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const exec = promisify(execOrig);
 
 export function getVersion() {
   // Uses readFileSync() instead of require() to prevent caching of values.
   const pkg = JSON.parse(readFileSync('./package.json'));
-  return pkg.version;
+  const prefix = process?.env?.BUILDTOOLS_VERSION_PREFIX || '';
+  return `${prefix}${pkg.version}`;
 }
 
 export function getReleaseBranch() {
